ef999a95506ed29eaa1247d31ea03d1ad3012024
Hugo Nicodemos <hugonicodemos@gmail.com>
2015-06-03 03:18:35 -0300

    Creating an interactive app

---
+++ js/controllers/MainController.js
@@ -0,0 +1,24 @@
+/**
+ * Created by nicodemos on 03/06/15.
+ */
+
+app.controller('MainController', ['$scope', function($scope) {
+    $scope.title = 'Top Sellers in Books';
+    $scope.promo = 'Promotion';
+    $scope.products = [
+        {
+            name: 'The Book of Trees',
+            price: 19,
+            pubdate: new Date('2014', '03', '08'),
+            cover: 'img/trees.jpg',
+            likes: 0
+        },
+        {
+            name: 'Program or be Programmed',
+            price: 8,
+            pubdate: new Date('2013', '08', '01'),
+            cover: 'img/program.jpg',
+            likes: 0
+        }
+    ]
+}]);
