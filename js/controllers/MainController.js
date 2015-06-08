7fe3980ab438c7255fadf8bcb5a3e4695a66bf37
Hugo Nicodemos <hugonicodemos@gmail.com>
2015-06-08 09:08:20 -0300

    add dislike button

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,16 +11,21 @@ app.controller('MainController', ['$scope', function($scope) {
             price: 19,
             pubdate: new Date('2014', '03', '08'),
             cover: 'img/trees.jpg',
-            likes: 0
+            likes: 0,
+            dislikes: 0
         },
         {
             name: 'Program or be Programmed',
             price: 8,
             pubdate: new Date('2013', '08', '01'),
             cover: 'img/program.jpg',
-            likes: 0
+            likes: 0,
+            dislikes: 0
         }
     ]
+    $scope.minusOne = function(index) {
+        $scope.products[index].dislikes += 1;
+    }
     $scope.plusOne = function(index) {
         $scope.products[index].likes += 1;
     }
